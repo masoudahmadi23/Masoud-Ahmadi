534a7bb8241fc6748a21e87118c71bfee13d3a58
Masoud Ahmadi <masoud23ahmadi@gmail.com>
2024-02-13 15:31:33 +0330

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// images
+import Logo from "../assets/logo.svg";
+
+const Header = () => {
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* logo */}
+          <div className="cursor-pointer">
+            <img src={Logo} alt="" />
+          </div>
+          {/* button */}
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
